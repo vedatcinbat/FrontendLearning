ac9b0e82a6f3e1d39091ddfeffa69b3437bd0776
vedat cinbat <cinbatvedat@gmail.com>
2024-09-05 11:10:01 +0300

    Introduction to TailwindCSS and Setting Up TailwindCSS in Next.js Done

---
+++ Tailwindcss/tailwind-practice/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-sky-300 p-3 text-center font-bold">Hello Next.js</div>
+    </>
+  );
+}
