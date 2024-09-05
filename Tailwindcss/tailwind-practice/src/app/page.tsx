6af4ee691972825e7117051e9a2e748607a9ef51
vedat cinbat <cinbatvedat@gmail.com>
2024-09-05 11:31:03 +0300

    Core Concepts and Tailwindcss Configuration Done

--- Tailwindcss/tailwind-practice/src/app/page.tsx
+++ Tailwindcss/tailwind-practice/src/app/page.tsx
@@ -4,6 +4,34 @@ export default function Home() {
   return (
     <>
       <div className="bg-sky-300 p-3 text-center font-bold">Hello Next.js</div>
+      <button
+        className="bg-blue-500 text-white p-4 rounded-lg"
+      >Button</button>
+      <div className="text-base md:text-lg lg:text-xl">Responsive Text</div> 
+      <div className="p-2 sm:p-4 lg:p-8 bg-sky-600">Responsive Padding</div>
+
+
+
+      {/* Customization */}
+      <div className="bg-customBlue text-customGray-light">
+        Custom Color Example
+      </div>
+      <div className="font-sans">
+        Using Inter Font
+      </div>
+      <div className="font-serif">
+        Using Merriweather Font
+      </div>
+
+      <div className="mt-72">
+        Marghin Top of 18rem
+      </div>
+
+      <div className="2xl:text-3xl">Text size changes at 1536px</div>
+
+      <div className="skew-10deg bg-blue-500 text-black p-4 rounded-lg">
+        Skewed Button
+      </div>
     </>
   );
 }
--- Tailwindcss/tailwind-practice/tailwind.config.ts
+++ Tailwindcss/tailwind-practice/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const plugin = require('tailwindcss/plugin');
 
 const config: Config = {
   content: [
@@ -8,12 +9,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px' // Adding a new breakpoint
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customBlue: '#1DA1F2',
+        customGray: {
+          light: '#f7fafc',
+          DEFAULT: '#a0aec0',
+          dark: '#718096',
+        }
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    //require('@tailwindcss/forms')
+    plugin(({addUtilities}) => {
+      const newUtilities = {
+        '.skew-10deg': {
+          transform: 'skewY(-10deg)',
+        },
+        '.skew-15deg': {
+          transform: 'skewY(-15deg)',
+        }
+      };
+
+      addUtilities(newUtilities);
+    })
+  ],
 };
 export default config;
