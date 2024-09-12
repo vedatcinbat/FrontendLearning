5d596643bfc576275d7697649d552e9f4d538c0e
Vedat Cinbat <cinbatvedat@gmail.com>
2024-09-12 22:31:18 +0300

    Advanced Nextjs App Created with Prettier Settings

---
+++ Nextjs/advanced-next/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Advanced Nextjs</h1>
+      <div>
+        safa sdsa asdasd fasd sd fas d s da sad fasd <h2>sdfasdfasd</h2>
+      </div>
+    </>
+  );
+}
---
+++ Nextjs/advanced-next/src/app/users/page.tsx
@@ -0,0 +1,18 @@
+export default function UsersPage() {
+  return (
+    <>
+      <h1>Users</h1>
+      <div>
+        <h2>Users</h2>
+      </div>
+
+      <div>
+        <h2>Users</h2>
+
+        <h2>Users</h2>
+        <h2>Users</h2>
+        <h2>Users</h2>
+      </div>
+    </>
+  );
+}
